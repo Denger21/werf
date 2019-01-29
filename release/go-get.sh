857a2b053fff5975bf6a7db7fdbb81bd127a8318
Timofey Kirillov <timofey.kirillov@flant.com>
2019-01-30 00:18:53 +0300

    CI: flant/werf-builder:1.0 go builder image

--- release/go-get.sh
+++ release/go-get.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-path=${GOPATH%%:*}/src
-
 for os in linux darwin windows ; do
   for arch in amd64 ; do
     export GOOS=$os
     export GOARCH=$arch
-    source go-get.sh
+    source $GOPATH/src/github.com/flant/werf/go-get.sh
   done
 done
